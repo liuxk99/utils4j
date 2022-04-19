ec75c8116055b3a71e7cb60af54145231c2bd9ff
Thomas Liu <liuxk99@gmail.com>
2022-04-20 06:59:41 +0800

    [Refactoring]: Extract methods.

--- src/main/java/com/sj4j/utils/DelayThread.java
+++ src/main/java/com/sj4j/utils/DelayThread.java
@@ -10,24 +10,30 @@ public class DelayThread extends Thread {
 
     @Override
     public void run() {
-        System.out.println(Thread.currentThread().getName() + ": =>" + "run()");
-        long targetTime = System.currentTimeMillis() + milliSeconds;
-        long delta = -1;
-        do {
-            long currentTime = System.currentTimeMillis();
-            delta = targetTime - currentTime;
-            if (delta > 0) {
-                try {
-                    Thread.sleep(delta);
-                } catch (InterruptedException e) {
-                    System.out.println(Thread.currentThread().getName() + ": Exception: " + e);
+        System.out.println(getTag() + "=> run()");
+        {
+            long targetTime = System.currentTimeMillis() + milliSeconds;
+            long delta = -1;
+            do {
+                long currentTime = System.currentTimeMillis();
+                delta = targetTime - currentTime;
+                if (delta > 0) {
+                    try {
+                        Thread.sleep(delta);
+                    } catch (InterruptedException e) {
+                        System.out.println(getTag() + "Exception: " + e);
                         e.printStackTrace(System.out);
+                    }
                 }
-            }
-        } while (delta >= 0);
+            } while (delta >= 0);
 
-        super.run();
+            super.run();
 
-        System.out.println(Thread.currentThread().getName() + ": <-" + "run()");
+        }
+        System.out.println(getTag() + "<- run()");
+    }
+
+    static String getTag() {
+        return String.format("[%s]: ", Thread.currentThread().getName());
     }
 }
